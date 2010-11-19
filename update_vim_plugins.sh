becd6cf1b3d26117dc859caf6fff49018f82c982
Samuel <samuelololol@gmail.com>
2010-11-19 17:28:01 +0800

    add crefvim

--- update_vim_plugins.sh
+++ update_vim_plugins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git submodule update --init
-for mod in nerdtree pydiction snipmate supertab gist-vim valgrind gtk-vim-syntax; do
+for mod in crefvim nerdtree pydiction snipmate supertab gist-vim valgrind gtk-vim-syntax; do
    cd $mod
    echo
    echo
