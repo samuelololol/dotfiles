585c780c32fd8a2245dc453dd257860f9cb1b1e3
Samuel <samuelololol@gmail.com>
2011-01-04 11:55:32 +0800

    upgrade supertab, gist-vim, and fixing upgrade_submodule.sh

--- upgrade_submodule.sh
+++ upgrade_submodule.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 for mod in crefvim nerdtree pydiction snipmate supertab gist-vim valgrind gtk-vim-syntax surround; do
    cd $mod
-   echo
-   echo
    echo "........"
    echo "module: $mod"
-   echo "........"
    git fetch
    git pull origin master
    cd ..
