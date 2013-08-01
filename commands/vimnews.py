47148753476e70926e19f969ba81aaf176488869
samuelololol <samuelololol@gmail.com>
2013-08-01 15:40:18 +0800

    change api server

--- commands/vimnews.py
+++ commands/vimnews.py
@@ -8,8 +8,10 @@ def cap(s,l):
     return s if len(s) <=l else s[0:l-3] + '...'
 
 def main():
-    x = urllib2.urlopen('http://gits-sac.appspot.com/vimplugins')
-    y = json.loads(x.read())
+    #x = urllib2.urlopen('http://gits-sac.appspot.com/vimplugins')
+    #y = json.loads(x.read())
+    x = urllib2.urlopen('http://samuel.rareodds.com/api/v1/vimplugins')
+    y = json.loads(x.read())['items']
     random.seed(time.time())
     z = y[random.randint(0,len(y))]
     print '%s %s' %(cap(z[1]+':'+z[2],50),z[0])
