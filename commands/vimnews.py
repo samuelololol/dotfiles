e99a7d803fd170378dc365e43812d70b740c5eac
samuelololol <samuelololol@gmail.com>
2013-08-20 17:42:13 +0800

    edit vimnews.py

--- commands/vimnews.py
+++ commands/vimnews.py
@@ -5,17 +5,17 @@ import json
 import random
 import time
 def cap(s,l):
-    return s if len(s) <=l else s[0:l-3] + '...'
+    return s if len(s) <=l else s[0:l-4] + ' ...'
 
 def main():
     try:
         #x = urllib2.urlopen('http://gits-sac.appspot.com/vimplugins')
         #y = json.loads(x.read())
-        x = urllib2.urlopen('http://samuel.rareodds.com/api/v1/vimplugins')
+        x = urllib2.urlopen('http://dev.samuelololol.org/api/v1/vimplugins')
         y = json.loads(x.read())['items']
         random.seed(time.time())
         z = y[random.randint(0,len(y))]
-        print '%s %s' %(cap(z[1]+':'+z[2],50),z[0])
+        print '%s' %(cap(z[0]+' : '+z[1],50))
     except Exception as e:
         print 'vimplugin_page: 404 [now]'
 
