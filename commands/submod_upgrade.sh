a129a38f9110bb19f3b0672bb1964e36a4fe55e7
samuelololol <samuelololol@gmail.com>
2016-09-14 01:16:05 +0800

    upgrade

--- commands/submod_upgrade.sh
+++ commands/submod_upgrade.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git submodule foreach --recursive git clean -fdx
-git submodule foreach --recursive git pull origin master
+#git submodule foreach --recursive git pull origin master
+git submodule foreach git pull origin master
