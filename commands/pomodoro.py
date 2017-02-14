fdacca02d6d099cb8c808c0a847eaf35b6559f5a
samuelololol <samuelololol@gmail.com>
2017-02-14 15:49:40 +0800

    update pomodoro scripts, default disable

--- commands/pomodoro.py
+++ commands/pomodoro.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 import time
 import sys
-import os
+# import os
 
 barlen = 20
+
+
 def percf(inp):
     if inp != -1:
         perc = int(inp * 100)
@@ -13,27 +15,29 @@ def percf(inp):
         elif perc > 80:
             return 'ALMOST!!!!'
         else:
-            return ''.join([str(perc),'%'])
+            return ''.join([str(perc), '%'])
     else:
         return ''
+
 
 def display(inp):
     if inp != -1:
         rtn = ''.join('[')
-        barc = int(barlen * inp) 
+        barc = int(barlen * inp)
         for i in range(barlen):
-            if  i < barc:
-                rtn = ''.join([rtn,'='])
+            if i < barc:
+                rtn = ''.join([rtn, '='])
             elif i == barc:
-                rtn = ''.join([rtn,'>'])
+                rtn = ''.join([rtn, '>'])
             else:
-                rtn = ''.join([rtn,' '])
-        rtn = ''.join([rtn,']'])
+                rtn = ''.join([rtn, ' '])
+        rtn = ''.join([rtn, ']'])
     else:
         rtn = ''.join(' Take a Break! ')
-        os.system('tmux display "You Should Take A Break!        \
-                  !Σ(  °д°lll)"')
+        # os.system('tmux display "You Should Take A Break!        \
+        #          !Σ(  °д°lll)"')
     return rtn
+
 
 def count25(min):
     if min > 30:
@@ -42,19 +46,21 @@ def count25(min):
         return -1
     else:
         rtn = (float(min) / 25)
-        return rtn 
+        return rtn
+
 
 def count50(min):
     if min > 49:
         return -1
     else:
         rtn = (float(min) / 50)
-        return rtn 
+        return rtn
+
 
 def main():
     min = int(time.ctime().split()[3].split(':')[1])
     unit = count50(min)   # [work,break] = [50,10]
-    #unit = count25(min)   # [work,break] = [25,5]
+    # unit = count25(min)   # [work,break] = [25,5]
     if len(sys.argv) != 1:
         if sys.argv[1] == 'perc':
             print percf(unit)
