b5c995e29bc5a917c9935d225e7f77ca69adc76e
samuelololol <samuelololol@gmail.com>
2017-07-31 15:16:41 +0800

    update update-scripts

--- commands/build_ycm.sh
+++ commands/build_ycm.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 cd ~/dotfiles/.vim/bundle/YouCompleteMe
-./install.py --clang-completer --gocode-completer
+if [ "$(uname)" == "Darwin" ]; then
+    echo "Build ycm in OS X environment ..."
+    ./install.py --clang-completer --gocode-completer --system-libclang --system-boost
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "Build ycm in Linux environment ..."
+    ./install.py --clang-completer --gocode-completer --system-libclang --system-boost
+fi
--- commands/submod_upgrade.sh
+++ commands/submod_upgrade.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+git submodule update --init --recursive --force
 git submodule foreach --recursive git clean -fdx
 git submodule foreach git pull origin HEAD
