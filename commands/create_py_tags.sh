2bc02f4d90b997fd3e4d542b66306eaf59b83684
samuelololol <samuelololol@gmail.com>
2018-10-30 13:41:24 +0800

    Update script

--- commands/create_py_tags.sh
+++ commands/create_py_tags.sh
@@ -6,36 +6,39 @@ fi
 PROJECT_ROOT_PATH=$1
 UNAME_STR=`uname`
 
-if [ $2 = "-f" ]; then
-    printf "Remove previous tag file\n"
-    rm -rf tags
-    printf "tags are removed\n"
-else
-    # remove previous
-    if [ -n "$ZSH_VERSION"  ]; then
-        printf "Would you like to remove previous tag file"
-        read -r " ? [Y/N]" response
+if [ -f tags ]; then
+    if [ $2 = "-f" ] && [ $# eq 2 ]; then
+        printf "Remove previous tag file\n"
+        rm -rf tags
+        printf "tags are removed\n"
     else
-        read -r -p " Would you like to remove previous tag file? [Y/N]" response
-    fi
+        # remove previous
+        if [ -n "$ZSH_VERSION"  ]; then
+            printf "Would you like to remove previous tag file"
+            read -r " ? [Y/N]" response
+        else
+            read -r -p " Would you like to remove previous tag file? [Y/N]" response
+        fi
 
-    # removing old tags
-    case "$response" in
-        [yY][eE][sS]|[yY])
-            rm -rf tags
-            printf "tags are removed\n"
-            ;;
-        *)
-            printf "tags are keeped\n"
-            ;;
-    esac
+        # removing old tags
+        case "$response" in
+            [yY][eE][sS]|[yY])
+                rm -rf tags
+                printf "tags are removed\n"
+                ;;
+            *)
+                printf "tags are keeped\n"
+                ;;
+        esac
+    fi
 fi
 
-ctags -R \
+ctags \
+-R \
 -h ".py" \
+--language-force=Python \
 --fields-Python=+{decorators} \
 --kinds-python=+v \
---language-force=Python \
 --exclude=@$HOME/.ctagsignore \
 --extra=+q *.py
 
@@ -49,7 +52,7 @@ else
 fi
 
 # statistics
-echo 'tags are generated'
+echo "tags are generated"
 TAG_COUNT=`wc -l tags | awk '{print $1}'`
 PY_FILE_COUNT=`find $PROJECT_ROOT_PATH -name "*.py" | wc -l | awk '{print $1}'`
 echo "generate "$TAG_COUNT" lines of tags from "$PY_FILE_COUNT" *.py files"
