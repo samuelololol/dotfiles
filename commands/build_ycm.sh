5b8a1b23e2922b445ee981bdcd37be67cec0fb27
samuelololol <samuelololol@gmail.com>
2019-01-28 22:54:05 +0800

    add karabiner settings
    add karabiner.json for karabiner-element for mac keyboard layout setting

--- commands/build_ycm.sh
+++ commands/build_ycm.sh
@@ -2,7 +2,8 @@
 cd ~/dotfiles/.vim/bundle/YouCompleteMe
 if [ "$(uname)" == "Darwin" ]; then
     echo "Build ycm in OS X environment ..."
-    ./install.py --clang-completer --gocode-completer --system-libclang --system-boost
+    #./install.py --clang-completer --gocode-completer --system-libclang --system-boost
+    ./install.py --clang-completer --system-libclang --system-boost
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Build ycm in Linux environment ..."
     ./install.py --clang-completer --gocode-completer --system-libclang --system-boost
