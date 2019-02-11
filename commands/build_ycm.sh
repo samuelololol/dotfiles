1d578542c19c3c6b746c903616c48925f2fe4f5d
samuelololol <samuelololol@gmail.com>
2019-02-11 11:31:06 +0800

    Update ycm build script for mac

--- commands/build_ycm.sh
+++ commands/build_ycm.sh
@@ -3,7 +3,13 @@ cd ~/dotfiles/.vim/bundle/YouCompleteMe
 if [ "$(uname)" == "Darwin" ]; then
     echo "Build ycm in OS X environment ..."
     #./install.py --clang-completer --gocode-completer --system-libclang --system-boost
+
+    #env
+    PATH="/usr/local/opt/llvm/bin:$PATH" \
+    LDFLAGS="-L/usr/local/opt/llvm/lib" \
+    CPPFLAGS="-I/usr/local/opt/llvm/include" \
     ./install.py --clang-completer --system-libclang --system-boost
+
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Build ycm in Linux environment ..."
     ./install.py --clang-completer --gocode-completer --system-libclang --system-boost
