be81e5ed1f64f9d77f6eff76b99d5cb926ab56db
samuelololol <samuelololol@gmail.com>
2022-03-15 10:24:33 +0800

    comment script on mac

--- commands/build_ycm.sh
+++ commands/build_ycm.sh
@@ -4,11 +4,11 @@ if [ "$(uname)" == "Darwin" ]; then
     echo "Build ycm in OS X environment ..."
     #./install.py --clang-completer --gocode-completer --system-libclang --system-boost
 
-    #env
-    PATH="/usr/local/opt/llvm/bin:$PATH" \
-    LDFLAGS="-L/usr/local/opt/llvm/lib" \
-    CPPFLAGS="-I/usr/local/opt/llvm/include" \
-    ./install.py --clang-completer --system-libclang --system-boost
+    ##env
+    #PATH="/usr/local/opt/llvm/bin:$PATH" \
+    #LDFLAGS="-L/usr/local/opt/llvm/lib" \
+    #CPPFLAGS="-I/usr/local/opt/llvm/include" \
+    python3 ./install.py --clang-completer --system-boost
 
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Build ycm in Linux environment ..."
